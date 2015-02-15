8ce31e085b5febe8c5e80748e4800dd1ce7dd172
Dez Iddon <dez@backpagecms.net>
2015-02-15 14:14:47 +0000

    Refactor of server.js.

--- server.js
+++ server.js
@@ -5,8 +5,4 @@
 *   About:  Simple point of interest mapping app
 *
 ****************************************************/
-'use strict';
-
-var simpl3s = require('simpl3s');
-
-simpl3s().init();
+require('simpl3s').init();
